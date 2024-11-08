cf437995884407233774affdbb0a6ddff2aa6b85
Nikita1814 <kravchenko1814@gmail.com>
2024-11-09 01:21:41 +0300

    feat: add delete, start implementing tracks

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { Response } from 'express';
 import { User } from 'src/interfaces/dbInterfaces';
@@ -22,12 +22,14 @@ uuidRegex = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-
   findUser(@Res() res: Response, @Param('id') id: string) {
     if (!this.uuidRegex.test(id)){
         res.status(HttpStatus.BAD_REQUEST).json({ message: 'the user id sent is not a valid UUID' });
+        return
     }
 
     const foundUser = this.dbService.database.users.find((user) => user.id === id)
 
     if (!foundUser){
         res.status(HttpStatus.NOT_FOUND).json({ message: 'User not found' });
+        return
     } else {
         const {password, ...rest} = foundUser
         res.status(HttpStatus.OK).json({...rest});
@@ -38,9 +40,10 @@ uuidRegex = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-
   @Post()
   create(@Body() createUserDto: CreateUserDto, @Res() res: Response ) {
 
-    if (!createUserDto.login || !createUserDto.password){
+    if (!createUserDto.login || !createUserDto.password) {
         res.status(HttpStatus.BAD_REQUEST).json({ message: 'Please provide a login and password for the user' });
-    }
+        return
+    } else {
     const newUser = {
         id: randomUUID(),
         login: createUserDto.login,
@@ -51,37 +54,63 @@ uuidRegex = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-
     }
     this.dbService.database.users.push(newUser);
     const {password, ...rest} = newUser
-    res.status(HttpStatus.OK).json({...rest});
+    res.status(HttpStatus.CREATED).json({...rest});
   }
+}
 
   @Put(':id')
   update(@Body() updateUserDto: UpdateUserDto, @Res() res: Response, @Param('id') id: string ) {
 
     if (!this.uuidRegex.test(id)){
         res.status(HttpStatus.BAD_REQUEST).json({ message: 'the user id sent is not a valid UUID' });
-    }
-
-    if (!updateUserDto.oldPassword || !updateUserDto.newPassword){
+        return
+    } else  if (!updateUserDto.oldPassword || !updateUserDto.newPassword){
         res.status(HttpStatus.BAD_REQUEST).json({ message: 'Please provide the old and new password for the user' });
-    }
-
+        return
+    } 
     const foundUser = this.dbService.database.users.find((user) => user.id === id);
 
     if (!foundUser){
         res.status(HttpStatus.NOT_FOUND).json({ message: 'User not found' });
+        return
     }
 
     const indexOfUser = this.dbService.database.users.findIndex((user) => user.id === id);
 
     if (!(this.dbService.database.users[indexOfUser].password === updateUserDto.oldPassword) ){
         res.status(HttpStatus.FORBIDDEN).json({ message: 'The old password does not match' });
-    }
+        return
+    } else {
 
 
 
     this.dbService.database.users[indexOfUser].password = updateUserDto.newPassword
     const {password, ...rest} = this.dbService.database.users[indexOfUser]
     res.status(HttpStatus.OK).json({...rest});
+    return
+    }
+  }
+
+  @Delete(':id')
+  delete(@Body() updateUserDto: UpdateUserDto, @Res() res: Response, @Param('id') id: string ) {
+
+    if (!this.uuidRegex.test(id)){
+        res.status(HttpStatus.BAD_REQUEST).json({ message: 'the user id sent is not a valid UUID' });
+        return
+    }
+
+    const foundUser = this.dbService.database.users.find((user) => user.id === id);
+
+    if (!foundUser){
+        res.status(HttpStatus.NOT_FOUND).json({ message: 'User not found' });
+        return
+    }
+
+    const indexOfUser = this.dbService.database.users.findIndex((user) => user.id === id);
+
+    const sliced = this.dbService.database.users.splice(indexOfUser,1)
+
+    res.status(HttpStatus.NO_CONTENT).json();
   }
 
 }
