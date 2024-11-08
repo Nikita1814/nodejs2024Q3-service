ef1621cf4f52ed1976c74701ad529a81bbc11dc7
Nikita1814 <kravchenko1814@gmail.com>
2024-11-09 01:35:57 +0300

    feat: implement tracks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TempDatabaseService } from './services/db.service';
 import { UserController } from './controllers/usersController';
+import { TrackController } from './controllers/tracksController';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
+  controllers: [AppController, UserController, TrackController],
   providers: [AppService, TempDatabaseService],
 })
 export class AppModule {}
--- src/controllers/tracksController.ts
+++ src/controllers/tracksController.ts
@@ -1,116 +1,107 @@
 import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { Response } from 'express';
-import { User } from 'src/interfaces/dbInterfaces';
-import { CreateUserDto, UpdateUserDto } from 'src/interfaces/userDtos';
+import { CrateTrackDto, UpdateTrackDto } from 'src/interfaces/trackDtos';
 import { TempDatabaseService } from 'src/services/db.service';
 
-// @Controller('track')
-// export class TrackController {
-// constructor(private readonly dbService: TempDatabaseService) {
+@Controller('track')
+export class TrackController {
+constructor(private readonly dbService: TempDatabaseService) {
 
-// }
+}
 
-// uuidRegex = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+uuidRegex = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
 
-//   @Get()
-//   findAll(@Res() res: Response) {
-//     res.status(HttpStatus.OK).json(this.dbService.database.users);
-//   }
+  @Get()
+  findAll(@Res() res: Response) {
+    res.status(HttpStatus.OK).json(this.dbService.database.tracks);
+  }
 
-//   @Get(':id')
-//   findUser(@Res() res: Response, @Param('id') id: string) {
-//     if (!this.uuidRegex.test(id)){
-//         res.status(HttpStatus.BAD_REQUEST).json({ message: 'the user id sent is not a valid UUID' });
-//         return
-//     }
+  @Get(':id')
+  findUser(@Res() res: Response, @Param('id') id: string) {
+    if (!this.uuidRegex.test(id)){
+        res.status(HttpStatus.BAD_REQUEST).json({ message: 'the track id sent is not a valid UUID' });
+        return
+    }
 
-//     const foundUser = this.dbService.database.users.find((user) => user.id === id)
+    const foundTrack = this.dbService.database.tracks.find((track) => track.id === id)
 
-//     if (!foundUser){
-//         res.status(HttpStatus.NOT_FOUND).json({ message: 'User not found' });
-//         return
-//     } else {
-//         const {password, ...rest} = foundUser
-//         res.status(HttpStatus.OK).json({...rest});
-//     }
-//   }
-
-
-//   @Post()
-//   create(@Body() createUserDto: CreateUserDto, @Res() res: Response ) {
-
-//     if (!createUserDto.login || !createUserDto.password) {
-//         res.status(HttpStatus.BAD_REQUEST).json({ message: 'Please provide a login and password for the user' });
-//         return
-//     } else {
-//     const newUser = {
-//         id: randomUUID(),
-//         login: createUserDto.login,
-//         password: createUserDto.password,
-//         version: 0,
-//         createdAt: new Date().getTime(),
-//         updatedAt: new Date().getTime(),
-//     }
-//     this.dbService.database.users.push(newUser);
-//     const {password, ...rest} = newUser
-//     res.status(HttpStatus.CREATED).json({...rest});
-//   }
-// }
-
-//   @Put(':id')
-//   update(@Body() updateUserDto: UpdateUserDto, @Res() res: Response, @Param('id') id: string ) {
-
-//     if (!this.uuidRegex.test(id)){
-//         res.status(HttpStatus.BAD_REQUEST).json({ message: 'the user id sent is not a valid UUID' });
-//         return
-//     } else  if (!updateUserDto.oldPassword || !updateUserDto.newPassword){
-//         res.status(HttpStatus.BAD_REQUEST).json({ message: 'Please provide the old and new password for the user' });
-//         return
-//     } 
-//     const foundUser = this.dbService.database.users.find((user) => user.id === id);
-
-//     if (!foundUser){
-//         res.status(HttpStatus.NOT_FOUND).json({ message: 'User not found' });
-//         return
-//     }
-
-//     const indexOfUser = this.dbService.database.users.findIndex((user) => user.id === id);
-
-//     if (!(this.dbService.database.users[indexOfUser].password === updateUserDto.oldPassword) ){
-//         res.status(HttpStatus.FORBIDDEN).json({ message: 'The old password does not match' });
-//         return
-//     } else {
+    if (!foundTrack){
+        res.status(HttpStatus.NOT_FOUND).json({ message: 'track not found' });
+        return
+    } else {
+        res.status(HttpStatus.OK).json(foundTrack);
+    }
+  }
 
 
+  @Post()
+  create(@Body() createTrackDto: CrateTrackDto, @Res() res: Response ) {
+/*
+    name: string;
+    artistId: string | null; // refers to Artist
+    albumId: string | null; // refers to Album
+    duration: number; // integer number
+*/
+    if (!createTrackDto.name || !createTrackDto.artistId || !createTrackDto.albumId || createTrackDto.duration ) {
+        res.status(HttpStatus.BAD_REQUEST).json({ message: 'Please provide all the fields for the track' });
+        return
+    } else {
+    const newTrack = {
+        id: randomUUID(),
+        ...createTrackDto
+    }
+    this.dbService.database.tracks.push(newTrack);
+    res.status(HttpStatus.CREATED).json(newTrack);
+  }
+}
 
-//     this.dbService.database.users[indexOfUser].password = updateUserDto.newPassword
-//     const {password, ...rest} = this.dbService.database.users[indexOfUser]
-//     res.status(HttpStatus.OK).json({...rest});
-//     return
-//     }
-//   }
+  @Put(':id')
+  update(@Body() updateTrackDto: UpdateTrackDto, @Res() res: Response, @Param('id') id: string ) {
 
-//   @Delete(':id')
-//   delete(@Body() updateUserDto: UpdateUserDto, @Res() res: Response, @Param('id') id: string ) {
+    if (!this.uuidRegex.test(id)){
+        res.status(HttpStatus.BAD_REQUEST).json({ message: 'the track id sent is not a valid UUID' });
+        return
+    } 
+    const foundTrack = this.dbService.database.tracks.find((track) => track.id === id);
 
-//     if (!this.uuidRegex.test(id)){
-//         res.status(HttpStatus.BAD_REQUEST).json({ message: 'the user id sent is not a valid UUID' });
-//         return
-//     }
+    if (!foundTrack){
+        res.status(HttpStatus.NOT_FOUND).json({ message: 'track not found' });
+        return
+    } else {
 
-//     const foundUser = this.dbService.database.users.find((user) => user.id === id);
+    const indexOfTrack = this.dbService.database.tracks.findIndex((track) => track.id === id);
+    
+    this.dbService.database.tracks[indexOfTrack] = {
+        ...this.dbService.database.tracks[indexOfTrack],
+        ...updateTrackDto
+    }
 
-//     if (!foundUser){
-//         res.status(HttpStatus.NOT_FOUND).json({ message: 'User not found' });
-//         return
-//     }
+    res.status(HttpStatus.OK).json( this.dbService.database.tracks[indexOfTrack]);
+    return
+    }
+  }
 
-//     const indexOfUser = this.dbService.database.users.findIndex((user) => user.id === id);
+  @Delete(':id')
+  delete(@Res() res: Response, @Param('id') id: string ) {
 
-//     const sliced = this.dbService.database.users.splice(indexOfUser,1)
+    if (!this.uuidRegex.test(id)){
+        res.status(HttpStatus.BAD_REQUEST).json({ message: 'The track id sent is not a valid UUID' });
+        return
+    }
 
-//     res.status(HttpStatus.NO_CONTENT).json();
-//   }
+    const foundTrack = this.dbService.database.tracks.find((track) => track.id === id);
 
-// }
+    if (!foundTrack){
+        res.status(HttpStatus.NOT_FOUND).json({ message: 'Track not found' });
+        return
+    }
+
+    const indexOfTrack = this.dbService.database.tracks.findIndex((user) => user.id === id);
+
+    const sliced = this.dbService.database.tracks.splice(indexOfTrack,1)
+
+    res.status(HttpStatus.NO_CONTENT).json();
+  }
+
+}
